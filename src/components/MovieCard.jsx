c37b639d07f928c33d8255ca96d8dfba241b3c66
Maria Liu <marialiu0220@gmail.com>
2026-08-08 22:03:19 -0400

    fix: handle no poster case

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,16 @@
 export const MovieCard = ({ movie }) => {
   return (
-    <div
-      key={movie.id}
-      className="max-w-sm p-6 rounded-lg shadow-md bg-slate-900"
-    >
+    <div className="max-w-sm p-6 rounded-lg shadow-md bg-slate-900">
       <h5 className="mb-2 text-lg font-bold tracking-tight text-white">
         {movie.title}
       </h5>
       <img
         className="block rounded-md"
-        src={`https://image.tmdb.org/t/p/w154${movie.poster_path}`}
+        src={
+          movie.poster_path
+            ? `https://image.tmdb.org/t/p/w154${movie.poster_path}`
+            : `https://placehold.co/154x231?text=No+Poster`
+        }
         alt={movie.title}
       />
     </div>
