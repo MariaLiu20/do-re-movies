ae472cc6c342f27214a56a8f0e71e9a9a92a6d7b
Maria Liu <marialiu0220@gmail.com>
2026-08-10 23:50:45 -0400

    build: install react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import "./App.css";
-import { MovieSearch } from "./components/MovieSearch";
+import { SearchPage } from "./components/SearchPage";
+import { MovieDetail } from "./components/MovieDetail";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
-    <div className="min-h-screen bg-slate-950 text-white font-sans">
-      {/* Header Container */}
-      <header className="max-w-4xl mx-auto pt-16 px-4 text-center">
-        <h1 className="text-4xl font-extrabold tracking-tight mb-4 text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-500">
-          DoReMovies
-        </h1>
-        <p className="text-slate-400 text-lg mb-8">
-          Discover millions of movies and TV shows.
-        </p>
-        <MovieSearch />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-slate-950 text-white font-sans">
+        <Routes>
+          <Route path="/" element={<SearchPage />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
