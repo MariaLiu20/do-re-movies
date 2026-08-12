4a97b3811140415b2a0c31f2657f417edc221aad
Maria Liu <marialiu0220@gmail.com>
2026-08-12 18:49:05 -0400

    feat: persist header/navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import "./App.css";
 import { MovieDetail } from "./components/MovieDetail";
 import { Home } from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router";
+import { SearchPage } from "./components/SearchPage";
+import { Header } from "./components/Header";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-slate-950 text-white font-sans">
+      <div className="min-h-screen bg-[#0a0a14] font-sans text-[12px]">
+        <Header />
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchPage />} />
           <Route path="/movie/:id" element={<MovieDetail />} />
           {/* <Route path="/watchlist" element={<WatchlistPage />} /> */}
         </Routes>
