97c4e8c95efc438a86e805af8f75a60ff1c9bdc4
Maria Liu <marialiu0220@gmail.com>
2026-08-12 21:02:06 -0400

    fix: no poster case

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,11 @@ export const MovieCard = ({ movie }) => {
     <div className="movie-card w-[162px]">
       <div className="relative h-[220px] overflow-hidden bg-[#050520]">
         <img
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          src={
+            movie.poster_path
+              ? `https://image.tmdb.org/t/p/w154${movie.poster_path}`
+              : `https://placehold.co/154x231?text=No+Poster`
+          }
           alt={movie.title}
           className="w-full h-full object-cover block"
         />
