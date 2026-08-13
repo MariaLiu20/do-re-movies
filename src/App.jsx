a65a943026cb63f7ccfd9f2b186c0e94b9253df0
Maria Liu <marialiu0220@gmail.com>
2026-08-12 21:22:36 -0400

    refactor: rename header file

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,17 @@ import { MovieDetail } from "./components/MovieDetail";
 import { Home } from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { SearchPage } from "./components/SearchPage";
-import { Header } from "./components/Header";
-import { NavBar } from "./components/NavBar";
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-[#0a0a14] font-sans text-[12px]">
-        <Header />
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/movie/:id" element={<MovieDetail />} />
-          {/* <Route path="/watchlist" element={<WatchlistPage />} /> */}
-        </Routes>
-      </div>
+      <Layout />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+      </Routes>
     </BrowserRouter>
   );
 }
